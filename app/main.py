ac57c23c014951554322af7f95521a8534310814
anastasiak <anastasiya.koryakina1@gmail.com>
2026-04-14 16:14:44 +0200

    Fix updated

--- app/main.py
+++ app/main.py
@@ -65,7 +65,7 @@ class Distance:
             return self.km >= other
         return NotImplemented
 
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         if isinstance(other, (int, float)):
